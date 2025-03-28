f3f2d15389d3af3a4cc1d5eea37b551e4f0dc8f9
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-28 20:27:40 +0530

    Operators

---
+++ Operator.java
@@ -0,0 +1,67 @@
+public class Operator {
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+
+
+    // Arithmetic Operator
+
+    // int res = num1 + num2;
+    // System.out.println("Addition: " + res);
+
+    // int res = num1 - num2;
+    // System.out.println("Subtraction: " + res);
+
+    // int res = num1 * num2;
+    // System.out.println("Multiplication: " + res);
+
+    int res = num1 / num2;
+    System.out.println("Division: " + res);
+
+    // int res = num1 % num2;
+    // System.out.println("Modulus: " + res);
+
+    // System.out.println(num1++);
+    // System.out.println(num1);
+    // System.out.println(++num1);
+
+    // System.out.println(--num1);
+    // System.out.println(num1--);
+    // System.out.println(num1);
+
+
+
+    //Comparision Operator
+
+
+    // System.out.println(num1 == num2);
+    // System.out.println(num1 != num2);
+    // System.out.println(num1 > num2);
+    // System.out.println(num1 < num2);
+    // System.out.println(num1 >= num2);
+    // System.out.println(num1 <= num2);
+    // System.out.println(num1 == num2 && num1 > num2);
+    // System.out.println(num1 == num2 || num1 > num2);
+
+
+
+    //Assignment Operator
+    int num3 = 12;
+    // num1 = num3;
+    // System.out.println(num1);
+
+    // num3+=12;
+    // System.out.println(num3);
+    // num3-=4;
+    // System.out.println(num3);
+    // num3*=3;
+    // System.out.println(num3);
+    // num3/=2;
+    // System.out.println(num3);
+    // num3%=2;
+    System.out.println(num3);
+
+    
+
+  }
+}
