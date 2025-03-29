0f432e06a38f069ec9985deaffd7f9c538acd789
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-29 16:22:56 +0530

    if Statement

---
+++ IfStatement.java
@@ -0,0 +1,14 @@
+class IfStatement {
+  public static void main(String[] args) {
+     int x = 10;
+     int y = 12;
+
+     if(x>y){
+      System.out.println("x is greater than y");
+     }
+
+     if(x<y){
+      System.out.println("x is less than y");
+     }
+  }
+}
