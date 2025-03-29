b92251a4c13c3f75ad4e3727bc6dcf758152e38d
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-30 00:17:34 +0530

    ternary Operator

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+  public static void main(String[] args) {
+    boolean isAdult = true;
+    String result = (isAdult == true) ? "You can vote" : "You can't vote";
+    System.out.println(result);
+  }
+}
