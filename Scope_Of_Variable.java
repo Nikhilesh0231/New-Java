733feace9fb79a9187b09cf80e90240b8a3f6504
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-29 15:53:04 +0530

    scope of variable

---
+++ Scope_Of_Variable.java
@@ -0,0 +1,18 @@
+public class Scope_Of_Variable {
+  public void display(){
+    int x = 10;
+    // System.out.println("inside display method x = " + x);
+  }
+  public static void main(String[] args) {
+
+    // System.out.println("inside class x = " + x);//we can not access the value of x from diplay method
+    int y = 12;
+    System.out.println("Inside class the value of y is : " + y);
+    {
+      int x = 20;
+      System.out.println("inside a block of x = " + x);
+    }
+    // System.out.println("Outside block value of x = "+x);//we can no access the value of out of that block in which it is declared.
+
+  }  
+}
