d51ef5048abdcc7362aea0c5c67f4c980d773fdd
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-30 14:52:29 +0530

    constructor

---
+++ constructors_in_java.java
@@ -0,0 +1,10 @@
+public class constructors_in_java {
+  //creating constructor
+  public constructors_in_java(){
+    System.out.println("Constructor is called.");
+  }
+  public static void main(String[] args) {
+    constructors_in_java obj = new constructors_in_java();
+    //After creating objects of the class contructor automatically called
+  }
+}
