414f43c5a73a712c900a3426278fb885b7afed9c
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-04-02 19:36:34 +0530

    instanceOfOperator

---
+++ instance_0f_keyword.java
@@ -0,0 +1,6 @@
+public class instance_0f_keyword {
+  public static void main(String[] args) {
+    instance_0f_keyword f1 = new instance_0f_keyword();
+    System.out.println(f1 instanceof instance_0f_keyword);//true
+  }
+}
