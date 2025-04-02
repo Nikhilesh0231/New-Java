6ee8f0957de62941f1197f4ddaf0bc059b1369fa
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-04-02 11:37:09 +0530

    Taking input from user using Scanner Class and Storing in array

---
+++ Arrays_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Arrays_3 {
+  public static void main(String[] args) {
+    int numOfElement ;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the number of elements in the array:");
+    numOfElement = input.nextInt();
+    int[] array = new int[numOfElement];
+    System.out.print("Enter the elements of the array:");
+    for(int i = 0; i < numOfElement; i++)
+    {
+      array[i] = input.nextInt();
+    }
+
+    System.out.println("Array Elements are : ");
+    for(int i : array){
+      System.out.print(i + " ");
+    }
+
+  }
+}
