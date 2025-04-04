1a64c842329c18ae3fd14496f2d1ae403b6981dc
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-04-04 13:03:26 +0530

    StringBuilder

--- reverseString.java
+++ reverseString.java
@@ -6,5 +6,17 @@ class reverseString{
       revStr = str.charAt(i) +revStr;
     }
     System.out.println(revStr);
+
+    StringBuilder sb = new StringBuilder("Hello");
+    sb.reverse();
+    System.out.println(sb);
+    sb.reverse();
+
+    sb.append("World");
+    System.out.println(sb);
+    sb.replace(5, sb.length(), "Veeru");
+    System.out.println(sb);
+    sb.reverse();
+    System.out.println(sb);
   }
 }
